bf416ec7a467bcb2bfbd9db7e3724f535bff0f6a
Mike <info@toolwheel.net>
2018-01-25 16:29:40 +0100

    Add new Team B including Buttons and count function

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,12 +7,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(0);
         displayForTeamA(0);
     }
 
@@ -46,6 +48,38 @@ public class MainActivity extends AppCompatActivity {
     public void submitCourt1ForTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * This method  is called when the Button +3 is clicked.
+     */
+    public void submitCourt3ForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method  is called when the Button +2 is clicked.
+     */
+    public void submitCourt2ForTeamB(View view) {
+        scoreTeamB = scoreTeamA + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method  is called when the Button +1 is clicked.
+     */
+    public void submitCourt1ForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
     }
 
 }
